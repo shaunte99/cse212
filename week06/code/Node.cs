c084bd8a13c424f0b99d7144b9332a91aeb5b777
shaunte99 <ksmatsetela99@gmail.com>
2025-08-05 21:12:45 +0200

    Update Node.cs

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -9,13 +9,15 @@ public class Node
         this.Data = data;
     }
 
+    // Problem 1: Insert only unique values
     public void Insert(int value)
     {
-        // TODO Start Problem 1
-
-        if (value < Data)
+        if (value == Data)
         {
-            // Insert to the left
+            return; // Ignore duplicates
+        }
+        else if (value < Data)
+        {
             if (Left is null)
                 Left = new Node(value);
             else
@@ -23,7 +25,6 @@ public class Node
         }
         else
         {
-            // Insert to the right
             if (Right is null)
                 Right = new Node(value);
             else
@@ -31,15 +32,23 @@ public class Node
         }
     }
 
+    // Problem 2: Contains
     public bool Contains(int value)
     {
-        // TODO Start Problem 2
-        return false;
+        if (value == Data)
+            return true;
+        else if (value < Data)
+            return Left != null && Left.Contains(value);
+        else
+            return Right != null && Right.Contains(value);
     }
 
+    // Problem 4: GetHeight
     public int GetHeight()
     {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = Left?.GetHeight() ?? 0;
+        int rightHeight = Right?.GetHeight() ?? 0;
+
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
